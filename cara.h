7061f69fb8860986205489975ef3e90a3495de3f
Szymon Urbaś <semahawk@hush.ai>
2012-03-03 21:08:18 +0100

    Add #ifndef CARA_H ... to cara.h

--- cara.h
+++ cara.h
@@ -4,10 +4,15 @@
  * Copyright: 2012 by Szymon Urbaś <urbas@hush.ai>
  */
 
+#ifndef CARA_H
+#define CARA_H
+
 #define VERSION "0.1.0"
 
 #define OP_NONE 0
 
 void usage(int op);
 static void version(void);
+
+#endif /* CARA_H */
 
